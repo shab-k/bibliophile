a5d9851bd65d0275fc7f2b82a3d0e52733214edc
shab-k <shab.k.1982@gmail.com>
2019-09-03 12:56:25 -0400

    fixed the mobile view of the reading list & new list

--- src/components/lists/B‌ookSummary.js
+++ src/components/lists/B‌ookSummary.js
@@ -7,7 +7,7 @@ const BookSummary = ({ book }) => {
   return (
     <div className="book card z-depth-0 book-summary">
       <div className="card-content grey-text text-darken-3">
-        <h5 className="card-title">Title: {book.title}</h5>
+        <h5 className="card-title">{book.title}</h5>
         <p className="details">Details <i className="fas fa-long-arrow-alt-right"></i></p>
         
       </div>
